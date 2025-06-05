ef61ee9b9855d4090d4ce5bea39c8478476e15ff
PRR Web Dev <prrwebdev@gmail.com>
2025-06-04 22:51:12 -0700

    Chore: update imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { initRevealAnimations } from './animations/revealAnimations';
-import { initCarousels } from './carousels';
-import { initFinsweetCmsSlider } from './finsweet/cmsSliderConfig';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+
 import { initCardStack } from './animations/cardStack';
+import { initRevealAnimations } from './animations/revealAnimations';
+import { initCarousels } from './carousels';
+
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener('DOMContentLoaded', () => {
